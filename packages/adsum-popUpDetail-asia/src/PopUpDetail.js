8432f5c6e7b7af0a5fa653439c47447dcc5cb354
Erlanda <erlandazakaria@gmail.com>
2019-04-12 14:30:59 +0700

    temporary notes

---
+++ packages/adsum-popUpDetail-asia/src/PopUpDetail.js
@@ -0,0 +1,31 @@
+// @flow
+
+import * as React from 'react';
+// import { connect } from 'react-redux';
+// import { PopUpDetailActions } from '..';
+
+/**
+ * PopUpDetail widget, display a carousel of medias (images or videos) or "Touch to Navigate" message
+ * @memberof module:PopUpDetail
+ * @class
+ * @extends React.Component
+ */
+
+// PopUpDetail -> show at POSITION (TOP, RIGHT, BOTTOM, LEFT), WIDTH, HEIGHT with Z-INDEX 15
+// Content -> (IN CONSIDERATION WHETHER INCLUDED HERE OR NOT)
+
+//ownprops: position(object{top,right,bottom,left})
+//          width, height (IN CONSIDERATION)
+//          children (CONTENT)
+
+export default class PopUpDetail extends React.Component<PropsType, StateType> {
+    static defaultProps = {
+
+    }
+
+    render(){
+        return(
+
+        )
+    }
+}
