3c1d377ad8e2c05799583bfe42d8a49fa149f3b5
Erlanda <erlandazakaria@gmail.com>
2019-04-12 11:04:43 +0700

    fix eslint

--- packages/adsum-screensaver-asia/src/ScreenSaverReducers.js
+++ packages/adsum-screensaver-asia/src/ScreenSaverReducers.js
@@ -1,23 +1,22 @@
 // @flow
 
-import { APP_CLICK } from './ScreenSaverActions';
-import { SCREEN_SAVER_CLOSE } from './ScreenSaverActions';
+import { APP_CLICK, SCREEN_SAVER_CLOSE } from './ScreenSaverActions';
 
 const initialState = {
     appClicked: false,
     screenSaverClose: false
-}
+};
 
 export default function (state = initialState, action) {
     switch (action.type) {
     case APP_CLICK:
         return Object.assign({}, state, {
             appClicked: action.payload
-          });
+        });
     case SCREEN_SAVER_CLOSE:
-    return Object.assign({}, state, {
-        screenSaverClose: action.payload
-      });
+        return Object.assign({}, state, {
+            screenSaverClose: action.payload
+        });
     default:
         return state;
     }
